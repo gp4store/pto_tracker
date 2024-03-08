b06d433b931e86c75497d74ad7f87f5763ee86c2
gp4store <getor3@gmail.com>
2024-03-08 04:48:27 -0800

    created pto_type, pto_request models register models on admin.py

--- track/admin.py
+++ track/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import pto_type, pto_request
 
+
+admin.site.register(pto_request)
+admin.site.register(pto_type)
 # Register your models here.
--- track/models.py
+++ track/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class pto_type(models.Model):
+
+    pto_type = models.CharField('Type', max_length = 3)
+
+    def __str__(self):
+        return self.pto_type
+    
+
+class pto_request(models.Model):
+
+    pto_type = models.ForeignKey(pto_type, blank = True, null = True, on_delete = models.CASCADE)
+    date_requested = models.DateTimeField(auto_now_add = True)
+    to = models.CharField('What are you using', max_length = 3, default = '')
+    hours_used = models.CharField('Hours used', max_length = 3)
+    notes = models.CharField('Notes', max_length = 120, blank = True)
+
+    def __str__(self):
+        return 'You requested ' + self.hours_used + ' hours on this date using ' + self.to
+    
