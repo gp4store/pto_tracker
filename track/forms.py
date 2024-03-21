ec309f8a8655a0cfbe370c750379b93def657f92
gp4store <getor3@gmail.com>
2024-03-21 02:54:12 -0700

    Added form-action and placeholders for new_req

--- track/forms.py
+++ track/forms.py
@@ -8,11 +8,19 @@ class pto_requestForm(ModelForm):
 
         model = pto_request
         fields = ('pto_type', 'date_requested', 'hours_used', 'send_to')
+        labels = {
+            
+            'pto_type': '',
+            'date_requested': '', 
+            'hours_used': '',
+            'send_to': '',
+        }
         widgets = {
-            'pto_type':
-            'date_requested':
-            'hours_used':
-            'send_to':
+            
+            'pto_type': forms.TextInput(attrs={'class':'form-control', 'placeholder':'What are you using?'}),
+            'date_requested': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Dates MM/DD/YY, '}),
+            'hours_used': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Hours to use'}),
+            'send_to': forms.EmailInput(attrs={'class':'form-control', 'placeholder':'send to?'}),
         }
 
 
